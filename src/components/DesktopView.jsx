40b686362781928f4a7c6a5429cc202f79edb2af
prolajumokeoni <prolajumokeoni@gmail.com>
2023-03-14 09:36:34 +0100

    Add side bar

--- src/components/DesktopView.jsx
+++ src/components/DesktopView.jsx
@@ -1,16 +1,35 @@
-import './Desktopview.styles.css'
-const DesktopView  = () => {
-	return (
-		<div  className={`${window.innerWidth >= 768 ? 'hidden sm:block' : 'sm:hidden' }`}>
-		<div className="flex h-screen">
-			<aside className="sidebar h- w-3/12">test</aside>
-				<main className="bg-gray w-9/12">
-				<nav className="bg-light">navigation</nav>
-				</main>
-		</div>
-		</div>
-	)
-}
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as SkippaLogo } from "../assets/skippa.svg";
+import "./Desktopview.styles.css";
 
+const DesktopView = () => {
+  return (
+    <div
+      className={`${
+        window.innerWidth >= 768 ? "hidden sm:block" : "sm:hidden"
+      }`}
+    >
+      <div className="flex h-screen">
+        <aside className="sidebar w-2/12">
+          <nav className="mt-10 w-10/12 mx-auto ">
+            <NavLink className="flex items-center">
+              <SkippaLogo />
+              <h1 className="text-light pl-3 font-extrabold">Skippa</h1>
+            </NavLink>
+
+            <NavLink className="flex items-center mt-24">
+              <UsersIcon className="h-6 w-6 text-gray" />
+              <p className="text-dark pl-3 font-semibold">Find Mentor</p>
+            </NavLink>
+          </nav>
+        </aside>
+        <main className="bg-gray w-10/12">
+          <nav className="bg-light">navigation</nav>
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default DesktopView;
